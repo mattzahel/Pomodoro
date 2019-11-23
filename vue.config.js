684d7d487791b3ac1e002b24ee69b4f307230c22
unknown <mateusz.zahel@gmail.com>
2019-11-23 18:57:57 +0100

    pwa base config, add icons

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pwa: {
+    name: 'Pomodoro Timer',
+    themeColor: '#313B4B',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    iconsPaths: {
+      favicon32: 'icons/favicon-32x32.png',
+      favicon16: 'icons/favicon-16x16.png',
+      appleTouchIcon: 'icons/icon-152x152.png',
+      // maskIcon: 'icons/safari-pinned-tab.svg',
+      msTileImage: 'icons/icon-144x144.png'
+    },
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'public/sw.js',
+      // ...other Workbox options...
+    }
+  }
+}
